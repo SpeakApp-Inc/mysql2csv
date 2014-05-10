f3e0096dc4570b9c00e933b80b24d4f2bf917838
Ricardo Lafuente <r@manufacturaindependente.org>
2014-05-10 23:46:35 +0100

    Make things more PEP8-y, change var names to more descriptive ones

--- mysql2csv.py
+++ mysql2csv.py
@@ -11,32 +11,32 @@ import click
 @click.option('-u', '--user', help='MySQL username', required=True)
 @click.option('-p', '--password', default="", help='MySQL password', required=True)
 @click.option('-d', '--dbname', help='Database name', required=True)
-@click.option('-t', '--table', default="#000000", help='Table or tables to fetch (for many tables, use commas without spaces, e.g. "table1,table2,table3")', required=False)
-def export(hostname,user,password,dbname,table):
-  '''Export a database into csv files.'''
+@click.option('-t', '--table', help='Table or tables to fetch (for many tables, use commas without spaces, e.g. "table1,table2,table3")', required=False)
+def export(hostname, user, password, dbname, table):
+    '''Export a database into csv files.'''
+  
+    if not password:
+        import getpass
+        password = getpass.getpass("Enter your password:")
+    
+    db = MySQLdb.connect(hostname, user, password, dbname)
+    tables = db.cursor()
+    tables.execute('SHOW TABLES')
 
-  if not password:
-    import getpass
-    password = getpass.getpass("Enter your password:")
+    if not os.path.isdir(dbname):
+        os.mkdir(dbname)
 
-  db=MySQLdb.connect(hostname,user,password,dbname)
-  tables=db.cursor()
-  tables.execute('SHOW TABLES')
-
-  if not os.path.isdir(dbname):
-    os.mkdir(dbname)
-
-  for table in tables:
-    print "Converting table %s...." % table
-    f=csv.writer(open(os.path.join(dbname,"%s.csv" % table),'w'))
-    colunas=db.cursor()
-    colunas.execute('DESCRIBE %s' % table)
-    f.writerow([i[0] for i in colunas])
-    dados=db.cursor()
-    dados.execute('SELECT * FROM %s' % table)
-    f.writerows(dados)
-    f.close()
+    for table in tables:
+        print "Converting table %s...." % table
+        f = csv.writer(open(os.path.join(dbname, "%s.csv" % table),'w'))
+        colnames = db.cursor()
+        colnames.execute('DESCRIBE %s' % table)
+        f.writerow([i[0] for i in colnames])
+        rows = db.cursor()
+        rows.execute('SELECT * FROM %s' % table)
+        f.writerows(rows)
+        f.close()
     
 if __name__=="__main__":
-  export()
+    export()
 
