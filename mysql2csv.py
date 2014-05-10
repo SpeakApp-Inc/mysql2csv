91320ad73af3063a9ab70f669c17fd59f3b85ab9
Ricardo Lafuente <r@manufacturaindependente.org>
2014-05-10 23:43:06 +0100

    Added python-click based CLI interface, as well as more verbose output

--- mysql2csv.py
+++ mysql2csv.py
@@ -4,11 +4,22 @@
 import sys, os, datetime
 import MySQLdb
 import csv
+import click
 
-def export(host,user,passwd,dbname):
+@click.command()
+@click.option('-h', '--hostname', default="127.0.0.1", help='Hostname')
+@click.option('-u', '--user', help='MySQL username', required=True)
+@click.option('-p', '--password', default="", help='MySQL password', required=True)
+@click.option('-d', '--dbname', help='Database name', required=True)
+@click.option('-t', '--table', default="#000000", help='Table or tables to fetch (for many tables, use commas without spaces, e.g. "table1,table2,table3")', required=False)
+def export(hostname,user,password,dbname,table):
   '''Export a database into csv files.'''
 
-  db=MySQLdb.connect(host,user,passwd,dbname)
+  if not password:
+    import getpass
+    password = getpass.getpass("Enter your password:")
+
+  db=MySQLdb.connect(hostname,user,password,dbname)
   tables=db.cursor()
   tables.execute('SHOW TABLES')
 
@@ -16,6 +27,7 @@ def export(host,user,passwd,dbname):
     os.mkdir(dbname)
 
   for table in tables:
+    print "Converting table %s...." % table
     f=csv.writer(open(os.path.join(dbname,"%s.csv" % table),'w'))
     colunas=db.cursor()
     colunas.execute('DESCRIBE %s' % table)
@@ -23,11 +35,8 @@ def export(host,user,passwd,dbname):
     dados=db.cursor()
     dados.execute('SELECT * FROM %s' % table)
     f.writerows(dados)
+    f.close()
     
 if __name__=="__main__":
-  if len(sys.argv)!=5:
-    print """Usage:
-    %s host user passwd db""" % sys.argv[0]
-    sys.exit(1)
-  export(*sys.argv[1:])
+  export()
 
