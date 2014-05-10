27c1e63da0219dbdf48da00792e146f11bac9c1b
Ricardo Lafuente <r@manufacturaindependente.org>
2014-05-11 00:50:28 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='mysql2csv',
+      version='0.2',
+      description='Convert some or all tables in a MySQL into CSV files.',
+      author='Elcio Ferreira',
+      author_email='elcio@visie.com.br',
+      maintainer='Ricardo Lafuente',
+      maintainer_email='r@manufacturaindependente.org',
+      url='http://github.com/rlafuente/mysql2csv/',
+      download_url='https://github.com/rlafuente/mysql2csv/tarball/master',
+      scripts=['mysql2csv'],
+      # license="GPL",
+     )
+
