07b0ad8a49c7a600ede3c1bd397ef16abb1c4d03
Ricardo Lafuente <r@manufacturaindependente.org>
2014-05-13 22:53:17 +0100

    Update setup for PyPI submission

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name='mysql2csv',
       version='0.2',
-      description='Convert some or all tables in a MySQL into CSV files.',
+      description='Convert some or all tables in a MySQL database into CSV files.',
       author='Elcio Ferreira',
       author_email='elcio@visie.com.br',
       maintainer='Ricardo Lafuente',
@@ -12,6 +12,7 @@ setup(name='mysql2csv',
       url='http://github.com/rlafuente/mysql2csv/',
       download_url='https://github.com/rlafuente/mysql2csv/tarball/master',
       scripts=['mysql2csv'],
-      # license="GPL",
+      keywords=['mysql', 'csv'],
+      license="MIT"
      )
 
