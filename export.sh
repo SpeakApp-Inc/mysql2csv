6c350a4d4ed4b6f9a79a356409e88dad08cd47f4
Eduardo Cuomo <reduardo7@gmail.com>
2019-07-12 14:22:42 -0300

    WIP: Docker Run

--- export.sh
+++ export.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+@error
+
 set -ex
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-# @TODO
-echo '@TODO' >&2
-exit 1
+./mysql2csv -q "SELECT * FROM vw_details_report;" -n details || echo 'Error generating Details Report' >&2
+./mysql2csv -q "CALL sp_summary(NULL);" -n summary || echo 'Error generating Summary Report' >&2
+./mysql2csv -q "CALL sp_university(NULL);" -n university || echo 'Error generating University Report' >&2
+
+echo 'Finish!'
